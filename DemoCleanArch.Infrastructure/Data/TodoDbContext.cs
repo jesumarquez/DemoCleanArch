5fe89e9791031016cdbc0e7cdcee9cade881673a
Jesú Márquez <jesumarquez@gmail.com>
2020-05-20 10:50:00 -0300

    savechangesasync save created and modified changes

--- DemoCleanArch.Infrastructure/Data/TodoDbContext.cs
+++ DemoCleanArch.Infrastructure/Data/TodoDbContext.cs
@@ -1,10 +1,13 @@
-﻿using DemoCleanArch.Domain.Entities;
+﻿using DemoCleanArch.Domain.Common;
+using DemoCleanArch.Domain.Entities;
 using DemoCleanArch.Infrastructure.Data.Configurations;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DemoCleanArch.Infrastructure.Data
 {
@@ -25,6 +28,27 @@ namespace DemoCleanArch.Infrastructure.Data
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<AuditEntity>())
+            {
+                //TODO: implementar current user
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = string.Empty;
+                        entry.Entity.Created = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = string.Empty;
+                        entry.Entity.LastModified = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
